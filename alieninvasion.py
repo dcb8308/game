498c31f2a580e1f70a0d9121808ee5ce6a9f7c79
DERRICK BELL <your_email@baylor.edu>
2025-06-26 18:49:09 -0500

    edits

--- alieninvasion.py
+++ alieninvasion.py
@@ -25,17 +25,37 @@ class AlienInvasion:
     def run_game(self):
         while True:
              self.check_events()
+             self.ship.update()
              self.update_screen()
 
 
     def _check_events(self):
          for event in pygame.event.get():
-             if event.type == pygame.QUIT:
+               if event.type == pygame.QUIT:
                   sys.exit()
-             elif event.type == pygame.KEYDOWN:
-                 if event.key == pygame.K_RIGHT:
-                     #move ship right
-                     self.ship.rect.x += 1
+            
+                elif event.type == pygame.KEYDOWN:
+                     self._check_keydown_events(event)
+                elif event.type == pygame.KEYUP:
+                     self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        if event.key == pygame.K_RIGHT:
+                self.ship.moving_right = True
+                self.ship.rect.x += 1
+        elif  event.key == pygame.K_LEFT:
+               self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+             sys.exit()
+             
+            
+             
+    def _check_keyup_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+                 self.ship.moving_left = False
+
             
 
 
