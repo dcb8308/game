b606438295cc9891fc55be1ab128b9bf1587cfc2
DERRICK BELL <your_email@baylor.edu>
2025-06-26 19:18:00 -0500

    bug fix game wouldnt load

--- alieninvasion.py
+++ alieninvasion.py
@@ -2,77 +2,74 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:
-
 
     def __init__(self):
         pygame.init()
         self.settings = Settings()
 
-        self.screen = pygame.display.set_mode(
-            (self.settings.screen_width, self.settings.screen_height))
-        
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
 
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
-        #set backround color#
+        # Set background color
         self.bg_color = (230, 230, 230)
-
 
     def run_game(self):
         while True:
-             self.check_events()
-             self.ship.update()
-             self.update_screen()
-
+            self._check_events()
+            self.ship.update()
+            self.bullets.update()
+            self._update_screen()
 
     def _check_events(self):
-         for event in pygame.event.get():
-               if event.type == pygame.QUIT:
-                  sys.exit()
-            
-                elif event.type == pygame.KEYDOWN:
-                     self._check_keydown_events(event)
-                elif event.type == pygame.KEYUP:
-                     self._check_keyup_events(event)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
 
     def _check_keydown_events(self, event):
         if event.key == pygame.K_RIGHT:
-                self.ship.moving_right = True
-                self.ship.rect.x += 1
-        elif  event.key == pygame.K_LEFT:
-               self.ship.moving_left = True
+            self.ship.moving_right = True
+            self.ship.rect.x += 1
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
         elif event.key == pygame.K_q:
-             sys.exit()
-             
-            
-             
+            sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
+
     def _check_keyup_events(self, event):
         if event.key == pygame.K_RIGHT:
             self.ship.moving_right = False
         elif event.key == pygame.K_LEFT:
-                 self.ship.moving_left = False
+            self.ship.moving_left = False
 
-            
+    def _fire_bullet(self):
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
 
-
-    def _updatde_screen(self):
-
+    def _update_screen(self):
         self.screen.fill(self.bg_color)
         self.ship.blitme()
-
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         pygame.display.flip()
-
-        
-
-    
 
 if __name__ == '__main__':
     # Make a game instance, and run the game.
     ai = AlienInvasion()
     ai.run_game()
+
 
 
--- ship.py
+++ ship.py
@@ -9,7 +9,7 @@ class Ship:
         self.screen_rect = ai_game.screen.get_rect()
 
      
-        self.image = pygame.image.load('images/ship.bmp')
+        self.image = pygame.image.load('/Users/Derrick/Documents/JS/game/ship.bmp')
         self.rect = self.image.get_rect()
 
         
